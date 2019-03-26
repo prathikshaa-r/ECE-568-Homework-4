ef4b1fa97932d991a02474d670b34ec9a0f3d4fb
prathikshaa-r <pratsr996@gmail.com>
2019-03-26 18:22:59 -0400

    cleanup, file structure & added __repr__ to print each object

---
+++ src/parser/server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+March 26, 2019
+Runs as a server connected on port 12345
+"""
+
+_author_= "Prathikshaa Rangarajan"
+_maintainer_ = "Prathikshaa Rangarajan"
+
+# ref: https://stackoverflow.com/questions/1603109/how-to-make-a-python-script-run-like-a-service-or-daemon-in-linux
+
+def open_server_socket():
+    
--- src/parser/xml_parser_header.py
+++ src/parser/xml_parser_header.py
@@ -9,10 +9,23 @@ _author_ = "Yanjia Zhao"
 _maintainer_ = "Prathikshaa Rangarajan"
 
 
+"""
+classes for Create_obj
+"""
+
 class Account:
     def __init__(self,id,balance):
         self.id=id
         self.balance=balance
+
+    """
+    print function
+    """
+    def __repr__(self):
+        print('Account ID: ', self.id)
+        print('Account Balance: ', self.balance)
+
+        return ''
 
         
 class Symbol:
@@ -20,12 +33,42 @@ class Symbol:
         self.sym=sym
         self.account=[] #the array of Account objects
 
+    """
+    print function
+    """
+    def __repr__(self):
+        print('Symbol: ', self.sym)
+        for accountInd in range (0, len(self.account)):
+            print('Symbol Account:\n', self.account[accountInd])
+            pass
+            
+        return ''
+
         
-class Create:
+class Create_obj:
     def __init__(self):
         self.account=[] #the array of Account objects
         self.symbol=[] #the array of Symbol objects
 
+    """
+    print function
+    """        
+    def __repr__(self):
+        print('Create:')
+        for symbolInd in range (0, len(self.symbol)):
+            print(self.symbol[symbolInd])
+            pass
+            
+        for accountInd in range (0, len(self.account)):
+            print(self.account[accountInd])
+            pass
+       
+        return ''
+
+
+"""
+classes for Transaction_obj
+"""
         
 class Order:
     def __init__(self,sym,amount,limit):
@@ -33,83 +76,149 @@ class Order:
         self.amount=amount
         self.limit=limit
 
+    """
+    print function
+    """        
+    def __repr__(self):
+
+        print('Order:')
+        print('Symbol: ', self.sym)
+        print('Amount: ', self.amount)
+        print('Limit: ', self.limit)
+
+        return ''
         
 class Query:
     def __init__(self,id):
         self.id=id
+
+    """
+    print function
+    """        
+    def __repr__(self):
+        print('Query ID: ', self.id)
+        return ''
 
         
 class Cancel:
     def __init__(self,id):
         self.id=id
 
+    """
+    print function
+    """        
+    def __repr__(self):
+        print('Cancel ID: ', self.id)
+        return ''
+
         
-class Transaction:
+class Transaction_obj:
     def __init__(self,id):
         self.id=id
         self.order=[] #the array of Order objects
         self.query=[] #the array of Query objects
         self.cancel=[] #array of Cancel objects
 
+    """
+    print function
+    """        
+    def __repr__(self):
+        print('Transaction ID: ', self.id)
+        for orderInd in range (0, len(self.order)):
+            print(self.order[orderInd])
+            pass
+            
+        for queryInd in range (0, len(self.query)):
+            print( self.query[queryInd])
+            pass
+
+        for cancelInd in range (0, len(self.cancel)):
+            print(self.cancel[cancelInd])
+            pass
+            
+        return ''
+
 # todo: convert below to a function
 # xml_parser("file.xml")
 import xml.etree.ElementTree as ET
-tree = ET.parse('../../test/country_data.xml')
-root = tree.getroot()
 
-#process create object here
-if root.tag=='create':
-    create=Create()
-    for child in root:
-        if child.tag=='account':
-            id1=child.attrib.get('id')
-            balance=child.attrib.get('balance')
-            account=Account(id1,balance)
-            create.account.append(account)
-        elif child.tag=='symbol':
-            symbol=child.attrib.get('sym')
-            sym=Symbol(symbol)
-            for grandchild in child:
-                id2=grandchild.attrib.get('id')
-                num=grandchild.text
-                account = Account(id2,num)
-                sym.account.append(account)
-            create.symbol.append(sym)
+def parse_xml():
+    tree = ET.parse('../../test/transaction_template.xml')
+    root = tree.getroot()
 
-            
-    for account in create.account:
-        print(account.id, account.balance)
-    for symbol in create.symbol:
-        print(symbol.sym)
-        for account in symbol.account:
-            print(account.id, account.balance)
+    #process create object here
+    if root.tag=='create':
+        create_obj=Create_obj()
+        for child in root:
+            if child.tag=='account':
+                id1=child.attrib.get('id')
+                balance=child.attrib.get('balance')
+                account=Account(id1,balance)
+                create_obj.account.append(account)
+                pass
 
-#process transaction object here
-if root.tag=='transactions':
-    id1=root.attrib.get('id')
-    transaction=Transaction(id1)
-    for child in root:
-        if child.tag=='order':
-            symbol=child.attrib.get('sym')
-            amount=child.attrib.get('amount')
-            limit = child.attrib.get('limit')
-            order=Order(symbol,amount,limit)
-            transaction.order.append(order)
-        elif child.tag=='query':
-            id2 = child.attrib.get('id')
-            query=Query(id2)
-            transaction.query.append(query)
-        elif child.tag=='cancel':
-            id2 = child.attrib.get('id')
-            cancel=Cancel(id2)
-            transaction.cancel.append(cancel)
+            elif child.tag=='symbol':
+                symbol=child.attrib.get('sym')
+                sym=Symbol(symbol)
+                for grandchild in child:
+                    id2=grandchild.attrib.get('id')
+                    num=grandchild.text
+                    account = Account(id2,num)
+                    sym.account.append(account)
+                    pass
+                create_obj.symbol.append(sym)
+                pass
+            pass
+        print(create_obj)
+                
+        # for account in create_obj.account:
+        #     print(account.id, account.balance)
 
-    print("Transaction ID: ", transaction.id)
-    for order in transaction.order:
-        print(order.sym,order.amount,order.limit)
-    for query in transaction.query:
-        print(query.id)
-    for cancel in transaction.cancel:
-        print(cancel.id)
+        # for symbol in create_obj.symbol:
+        #     print(symbol.sym)
+        #     for account in symbol.account:
+        #         print(account.id, account.balance)
+        pass
 
+    #process transaction object here
+    if root.tag=='transactions':
+        id1=root.attrib.get('id')
+        transaction_obj=Transaction_obj(id1)
+        for child in root:
+            if child.tag=='order':
+                symbol=child.attrib.get('sym')
+                amount=child.attrib.get('amount')
+                limit = child.attrib.get('limit')
+                order=Order(symbol,amount,limit)
+                transaction_obj.order.append(order)
+                pass
+
+            elif child.tag=='query':
+                id2 = child.attrib.get('id')
+                query=Query(id2)
+                transaction_obj.query.append(query)
+                pass
+
+            elif child.tag=='cancel':
+                id2 = child.attrib.get('id')
+                cancel=Cancel(id2)
+                transaction_obj.cancel.append(cancel)
+                pass
+            pass
+
+        print(transaction_obj)
+
+    #     print("Transaction_Obj ID: ", transaction_obj.id)
+    #     for order in transaction_obj.order:
+    #         print(order.sym,order.amount,order.limit)
+    #     for query in transaction_obj.query:
+    #         print(query.id)
+    #     for cancel in transaction_obj.cancel:
+    #         print(cancel.id)
+    #         pass
+    #     pass
+    # pass
+
+
+parse_xml()
 
