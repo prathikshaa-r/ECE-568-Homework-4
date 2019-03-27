5bc8c5adfff68186ce2ec491b0c08759d383dc8e
yz476 <yz476@duke.edu>
2019-03-27 18:16:30 -0400

    socket_code_v2

---
+++ src/socket_version2/server.py
@@ -0,0 +1,35 @@
+#socket_echo_server.py                                                                                           
+import socket
+import sys
+
+# Create a TCP/IP socket                                                                                         
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port                                                                                    
+server_address = (socket.gethostname(), 10000)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+# Listen for incoming connections                                                                                
+sock.listen(1)
+
+while True:
+    # Wait for a connection                                                                                      
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+
+        # Receive the data in small chunks and retransmit it                                                     
+        with open('received_file','wb') as f:
+            while True:
+                data = connection.recv(1024)
+                print('received {!r}'.format(data))
+                if data:
+                    """                                                                                          
+                    print('sending data back to the client')                                                     
+                    connection.sendall(data)                                                                     
+                    """
+                else:
+                    break
+                f.write(data)
