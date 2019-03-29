99bd3d48888405aa684f3567c3de7a680a744834
yz476 <yz476@duke.edu>
2019-03-29 16:30:22 -0400

    multi-threading

--- src/socket_version2/server.py
+++ src/socket_version2/server.py
@@ -1,8 +1,27 @@
 #socket_echo_server.py                                                                                           
 import socket
 import sys
+import threading
 sys.path.append('../parser')
 from xml_parser_header import parse_xml
+
+def process_request(connection, client_address):
+    try:
+        print('connection from', client_address)
+        # Receive the data in small chunks and retransmit it
+        recv_string=""
+        while True:
+            data = connection.recv(1024)
+            if data:
+                recv_string += data.decode("utf-8")
+            else:
+                break
+        parse_xml(recv_string)
+    finally:
+        # Clean up the connection
+        connection.close()
+
+
 
 # Create a TCP/IP socket                                                                                         
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,6 +38,9 @@ while True:
     # Wait for a connection                                                                                      
     print('waiting for a connection')
     connection, client_address = sock.accept()
+    t=threading.Thread(target=process_request(connection,client_address))
+    t.start()
+    """
     try:
         print('connection from', client_address)
         # Receive the data in small chunks and retransmit it
@@ -26,10 +48,10 @@ while True:
         while True:
             data = connection.recv(1024)
             if data:
-                """                                                                                          
-                print('sending data back to the client')                                                     
-                connection.sendall(data)                                                                     
-                """
+                                                                                                          
+                #print('sending data back to the client')                                                     
+                #connection.sendall(data)                                                                     
+                
             else:
                 break
             recv_string+=data.decode("utf-8")
@@ -37,3 +59,4 @@ while True:
     finally:
         # Clean up the connection
         connection.close()
+    """
