3da65e5a1ec1c12cd9da207a11c5234074f5edb1
yz476 <yz476@duke.edu>
2019-03-28 20:43:30 -0400

    transaction_resp_test

--- src/parser/response.py
+++ src/parser/response.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from xml.etree.ElementTree import Element, SubElement
 from ElementTree_pretty import prettify
-from xml_parser_header import Create_obj,Account,Position
-from Response_obj import Sub_Response, Response
+from xml_parser_header import Create_obj,Account,Position, Order
+from response_obj import Order_resp,TransactionResponse,TransactionSubResponse
 
 ##response of create
 def create_response(create):#create=Create_obj()
@@ -31,10 +31,10 @@ def create_response(create):#create=Create_obj()
 
 
 ##response of transaction
-#transaction=Transaction_obj(11)
-def transaction_response(transaction):
+#response
+def transaction_response(response):
     top=Element('results')
-    for child in transaction.sequence:
+    for child in response:
         if child.type=='order':
             if (child.open):
                 attributes={"sym":child.sym, "amount":child.amount, "limit":child.limit, "id":child.id}
@@ -43,33 +43,24 @@ def transaction_response(transaction):
                 attributes={"sym":child.sym, "amount":child.amount, "limit":child.limit}
                 node=SubElement(top,'error',attributes)
                 node.text=child.msg
-        
-        if child.type=='query':
-            # I want to get one Response object here according to child.id
-            response=Response() # assumen this is the response object that I get
-            node=SubElement(top,'status')
-            for sub_resp in response.response:
-                if sub_resp.status=="open":
-                    attributes = {"shares": sub_resp.shares}
-                    subnode=SubElement(node,'open',attributes)
-                if sub_resp.status=="canceled":
-                    attributes= {"shares": sub_resp.shares,"time": sub_resp.time}
-                    subnode = SubElement(node, 'canceled', attributes)
-                if sub_resp.status=="executed":
-                    attributes = {"shares": sub_resp.shares,"price":sub_resp.price, "time": sub_resp.time}
-                    subnode = SubElement(node, 'executed', attributes)
-        if child.type=='cancel':
-            # I want to get one Response object here according to child.id
-            # After all the open orders with this child.id is canceled
-            response = Response()  # assumen this is the response object that I get
-            node = SubElement(top, 'canceled')
-            for sub_resp in response.response:
-                if sub_resp.status == "canceled":
-                    attributes = {"shares": sub_resp.shares, "time": sub_resp.time}
-                    subnode = SubElement(node, 'canceled', attributes)
-                if sub_resp.status == "executed":
-                    attributes = {"shares": sub_resp.shares, "price": sub_resp.price, "time": sub_resp.time}
-                    subnode = SubElement(node, 'executed', attributes)
+        if child.type=='transac':
+            attributes={"id":child.trans_id}
+            node
+            if child.request=='query':
+                node=SubElement(top,'status',attributes)
+            if child.request=='cancel':
+                node=SubElement(top,'canceled',attributes)
+            for grand_child in child.trans_resp:
+                if(grand_child.status=='open'):
+                    sub_attributes = {"shares": grand_child.shares}
+                    subnode = SubElement(node, 'open', sub_attributes)
+                if(grand_child.status=='canceled'):
+                    sub_attributes = {"shares": grand_child.shares, "time": grand_child.time}
+                    subnode = SubElement(node, 'canceled', sub_attributes)
+                if(grand_child.status=='executed'):
+                    sub_attributes = {"shares": grand_child.shares, "price": grand_child.price, "time": grand_child.time}
+                    subnode = SubElement(node, 'executed', sub_attributes)
+    print(prettify(top))
 
 """
 create_response() test
@@ -88,6 +79,35 @@ create.sequence.append(position1)
 create.sequence.append(position2)
 create_response(create)
 """
+
+
+"""
+transaction_response() test
+"""
+"""
+order1=Order('DUKE','3000','4000')
+Order1=Order_resp(order1,True,'open','1')
+order2=Order('UNC','300','400')
+Order2=Order_resp(order2,False,'err','2')
+trs1=TransactionSubResponse('open','300','300','300')
+trs2=TransactionSubResponse('canceled','20','20','20')
+trs3=TransactionSubResponse('executed','66','66','66')
+tr1=TransactionResponse('1','query')
+tr1.trans_resp.append(trs1)
+tr1.trans_resp.append(trs2)
+tr1.trans_resp.append(trs3)
+tr2=TransactionResponse('2','cancel')
+tr2.trans_resp.append(trs2)
+tr2.trans_resp.append(trs3)
+response=[]
+response.append(Order1)
+response.append(Order2)
+response.append(tr1)
+response.append(tr2)
+transaction_response(response)
+
+"""
+
 
 
 
